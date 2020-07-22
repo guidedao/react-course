cec38c478e8fac1493ed979f517adbc4b3e11a26
Moscow Coding School <yo@moscoding.ru>
2020-07-22 18:29:46 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Moscow Coding School</h1>
+      <h2>Курс «Реакт шаг за шагом»</h2>
+    </div>
+  );
+}
